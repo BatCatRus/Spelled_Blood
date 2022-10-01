99aa99807ddd75dfbc3ea9396ed46134cb20e3da
Даня <112954815+BatCatRus@users.noreply.github.com>
2022-10-02 01:41:49 +0300

    0.0.3
    Начал тестировать приколюхи

--- DemoSB/Assets/Skripts/WalkedMonster.cs
+++ DemoSB/Assets/Skripts/WalkedMonster.cs
@@ -8,9 +8,13 @@ public class WalkedMonster : Entity
     private Vector3 dir;
     private SpriteRenderer sprite;
 
+    private void Awake()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
     private void Start()
     {
-        sprite.flipX = dir.x < 0.0f;
         dir = transform.right;
     }
 
@@ -21,20 +25,16 @@ public class WalkedMonster : Entity
 
     private void Move()
     {
-        sprite.flipX = dir.x < 0.0f;
+        
 
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.1f + transform.right * dir.x * 0.7f, 0.1f);
-
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.1f + transform.right * dir.x * 0.7f, -0.1f);
+        
         if (colliders.Length > 0) dir *= -1f;
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, Time.deltaTime);
-
+        sprite.flipX = dir.x < 0.0f;
     }
 
-    private void Awake()
-    {
-        sprite = GetComponentInChildren<SpriteRenderer>();
-    }
-
+  
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject == Hero.Instance.gameObject)
