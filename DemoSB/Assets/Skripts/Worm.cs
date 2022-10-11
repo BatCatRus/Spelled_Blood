9cfecbd1eefba0460853b2525a17cb6614e5567c
Даня <112954815+BatCatRus@users.noreply.github.com>
2022-10-12 01:19:37 +0300

    0.0.7
    Ура урон работает, можно убивать

--- DemoSB/Assets/Skripts/Worm.cs
+++ DemoSB/Assets/Skripts/Worm.cs
@@ -9,16 +9,16 @@ public class Worm : Entity
         lives = 3;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject == Hero.Instance.gameObject)
-        {
-            Hero.Instance.GetDamage();
-            lives--;
-            Debug.Log("у Сливы " + lives);
-        }
-
-        if (lives < 1)
-            Die();
-    }
+ //       private void OnCollisionEnter2D(Collision2D collision)
+ //       {
+ //           if (collision.gameObject == Hero.Instance.gameObject)
+ //           {
+ //               Hero.Instance.GetDamage();
+ //               lives--;
+ //               Debug.Log("у херни " + lives);
+ //           }
+//
+ //           if (lives < 1)
+ //               Die();
+ //       }
 }
