9eb6b1190b1f7c5adc46a0a9f7c4a26ee222606f
Даня <112954815+BatCatRus@users.noreply.github.com>
2023-01-05 03:56:12 +0300

    0.8.7
    Добавил нанесение урона противникам + анимацию нанесения урона + анимация смерти.

---
+++ DemoSB/Assets/Enemy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+    
+    public int maxHealth = 100;
+    int currentHealth;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        animator.SetTrigger("Hurt");
+
+        // Play hurt animation
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+
+        // Die animation
+
+        animator.SetBool("IsDead", true);
+
+        // Disable the enemy
+
+        this.enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+    }
+}
---
+++ DemoSB/Assets/Skripts/PlayerCombat.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float attackRange = 0.5f;
+    public int attackDamage = 10;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown("r"))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        // Play an attack animation
+        animator.SetTrigger("Attack");
+
+        // Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+
+    }
+}
