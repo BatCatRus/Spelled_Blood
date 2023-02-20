4912a473f58d20e33760758d3d470a83e566bb2e
Даня <112954815+BatCatRus@users.noreply.github.com>
2023-02-20 12:33:31 +0300

    0.0.9.1

--- CombatBatyanya/Assets/Sripts/PlayerController2d.cs
+++ CombatBatyanya/Assets/Sripts/PlayerController2d.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController2d : MonoBehaviour
+
+    public float movementForce;
+    public float maxHorizontalMovementSpeed
+
+    private float movementDirection;
+    private Rigidbody2D rb;
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
