ff2778bea548ab9d66a2240bc796376e43b7f19f
Ghost7133 <112954815+BatCatRus@users.noreply.github.com>
2023-02-21 14:37:15 +0300

    0.0.9.4

--- CombatBatyanya/Assets/Sripts/PlayerController2d.cs
+++ CombatBatyanya/Assets/Sripts/PlayerController2d.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController2d : MonoBehaviour
-
+{ 
     public float movementForce;
-    public float maxHorizontalMovementSpeed
+    public float maxHorizontalMovementSpeed;
 
     private float movementDirection;
     private Rigidbody2D rb;
-{
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +18,12 @@ public class PlayerController2d : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
+        {
+            movementDirection = Input.GetAxis("Horizontal");
+        }
+    private void FixedUpdate()
+        {
+            Vector2 forceToAdd = new Vector2(movementForce * movementDirection, 0);
+            rb.AddForce(forceToAdd);
+        }
 }
