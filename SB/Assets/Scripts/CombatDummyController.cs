bbe1dfe55e385487e0f68fd228606da7a5025101
Даня <112954815+BatCatRus@users.noreply.github.com>
2023-03-08 02:54:18 +0300

    0.2.2.2
    Dunny death

--- SB/Assets/Scripts/CombatDummyController.cs
+++ SB/Assets/Scripts/CombatDummyController.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class CombatDummyController : MonoBehaviour
 {
-    [Serializefield]
-    private float maxHealth;
+    [SerializeField]
+    private float maxHealth, knockbackSpeedX, knockbackSpeedY, knockbackDuration, knockbackDeathSpeedX, knockbackDeathSpeedY, deathTorque;
+    [SerializeField]
+    private bool applyKnockback;
+    [SerializeField]
+    private GameObject HitParticle;
 
-    private float currentHealth;
+    private float currentHealth, knockbackStart;
+
+    private int playerFacingDirection;
+
+    private bool playerOnLeft, knockback;
 
     private PlayerController2D pc;
-    private GameObject aliveGO, brokenTopGO, brokenBotGo;
+    private GameObject aliveGO, brokenTopGO, brokenBotGO;
     private Rigidbody2D rbAlive, rbBrokenTop, rbBrokenBot;
     private Animator aliveAnim;
 
-
-    private void start()
+    private void Start()
     {
         currentHealth = maxHealth;
 
-        pc = GameObject.Find("Player").GetComponent<PlayerController>();
+        pc = GameObject.Find("Player").GetComponent<PlayerController2D>();
 
         aliveGO = transform.Find("Alive").gameObject;
         brokenTopGO = transform.Find("Broken Top").gameObject;
@@ -29,5 +36,76 @@ public class CombatDummyController : MonoBehaviour
         rbAlive = aliveGO.GetComponent<Rigidbody2D>();
         rbBrokenTop = brokenTopGO.GetComponent<Rigidbody2D>();
         rbBrokenBot = brokenBotGO.GetComponent<Rigidbody2D>();
+
+        aliveGO.SetActive(true);
+        brokenTopGO.SetActive(false);
+        brokenBotGO.SetActive(false);
+    }
+
+    private void Update()
+    {
+        CheckKnockback();
+    }
+
+    private void Damage(float amount)
+    {
+        currentHealth -= amount;
+        playerFacingDirection = pc.GetFacingDirection();
+
+        Instantiate(HitParticle, aliveGO.transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
+
+        if (playerFacingDirection == 1)
+        {
+            playerOnLeft = true;
+        }
+        else
+        {
+            playerOnLeft = false;
+        }
+
+        aliveAnim.SetBool("playerOnLeft", playerOnLeft);
+        aliveAnim.SetTrigger("damage");
+
+        if (applyKnockback && currentHealth > 0.0f)
+        {
+            //Knockback
+            Knockback();
+        }
+
+        if (currentHealth <= 0.0f)
+        {
+            //Die
+            Die();
+        }
+    }
+
+    private void Knockback()
+    {
+        knockback = true;
+        knockbackStart = Time.time;
+        rbAlive.velocity = new Vector2(knockbackSpeedX * playerFacingDirection, knockbackSpeedY);
+    }
+
+    private void CheckKnockback()
+    {
+        if (Time.time >= knockbackStart + knockbackDuration && knockback)
+        {
+            knockback = false;
+            rbAlive.velocity = new Vector2(0.0f, rbAlive.velocity.y);
+        }
+    }
+
+    private void Die()
+    {
+        aliveGO.SetActive(false);
+        brokenTopGO.SetActive(true);
+        brokenBotGO.SetActive(true);
+
+        brokenTopGO.transform.position = aliveGO.transform.position;
+        brokenBotGO.transform.position = aliveGO.transform.position;
+
+        rbBrokenBot.velocity = new Vector2(knockbackSpeedX * playerFacingDirection, knockbackSpeedY);
+        rbBrokenTop.velocity = new Vector2(knockbackDeathSpeedX * playerFacingDirection, knockbackDeathSpeedY);
+        rbBrokenTop.AddTorque(deathTorque * -playerFacingDirection, ForceMode2D.Impulse);
     }
 }
